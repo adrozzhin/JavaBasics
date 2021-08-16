da2aa78335d5b66ecb308301a5612fb69762a832
adrozzhin <drozzhinandrei@gmail.com>
2021-08-15 18:20:43 -0700

    Added advanced package

---
+++ src/com/syntax/advancedjava/TicTacToe.java
@@ -0,0 +1,90 @@
+package com.syntax.advancedjava;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char[][] gameBoard = { { ' ', '|', ' ', '|', ' ' }, 
+								{ '-', '+', '-', '+', '-' }, 
+								{ ' ', '|', ' ', '|', ' ' },
+								{ '-', '+', '-', '+', '-' }, 
+								{ ' ', '|', ' ', '|', ' ' } };
+
+		printGameBoard(gameBoard);
+		
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please choose a placement from 1 to 9:");
+		int pos = scan.nextInt();
+		System.out.println(pos);
+		
+		placePiece(gameBoard, pos, "player");
+		printGameBoard(gameBoard);
+		
+		Random rand = new Random();
+		int cpuPos = rand.nextInt(9) + 1;
+		placePiece(gameBoard, cpuPos, "cpu");
+		
+		System.out.println("Bot's turn:");
+		printGameBoard(gameBoard);
+		
+
+	}
+
+	public static void printGameBoard(char[][] gameBoard) {
+		for (char[] row : gameBoard) {
+			for (char column : row) {
+				System.out.print(column);
+			}
+			System.out.println();
+
+		}
+	}
+	public static void placePiece(char[][] gameBoard, int pos, String user) {
+		
+		char symbol = ' ';
+		
+		if (user.equals("player")) {
+			symbol = 'X';
+		} else if (user.equals("cpu")){
+			symbol = 'O';
+		}
+		
+		switch (pos) {
+		case 1:
+			gameBoard[0][0] = symbol;
+			break;
+		case 2:
+			gameBoard[0][2] = symbol;
+			break;
+		case 3:
+			gameBoard[0][4] = symbol;
+			break;
+		case 4:
+			gameBoard[2][0] = symbol;
+			break;
+		case 5:
+			gameBoard[2][2] = symbol;
+			break;
+		case 6:
+			gameBoard[2][4] = symbol;
+			break;
+		case 7:
+			gameBoard[4][0] = symbol;
+			break;
+		case 8:
+			gameBoard[4][2] = symbol;
+			break;
+		case 9:
+			gameBoard[4][4] = symbol;
+			break;
+		default:
+			break;
+		}
+	}
+	
+}
