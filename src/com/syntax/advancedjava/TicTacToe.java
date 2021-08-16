b8716754052c9264ccd5931deef864831efaa07b
adrozzhin <drozzhinandrei@gmail.com>
2021-08-15 18:57:53 -0700

    modified TicTacToe

--- src/com/syntax/advancedjava/TicTacToe.java
+++ src/com/syntax/advancedjava/TicTacToe.java
@@ -1,9 +1,14 @@
 package com.syntax.advancedjava;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
-public class TicTacToe {
+public class TicTacToe { 
+	
+	static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -16,21 +21,26 @@ public class TicTacToe {
 
 		printGameBoard(gameBoard);
 		
+
 		
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Please choose a placement from 1 to 9:");
-		int pos = scan.nextInt();
-		System.out.println(pos);
+		while(true) {
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Please choose a placement from 1 to 9:");
+			int pos = scan.nextInt();
+			System.out.println(pos);
+			
+			placePiece(gameBoard, pos, "player");
+			printGameBoard(gameBoard);
+			
+			Random rand = new Random();
+			int cpuPos = rand.nextInt(9) + 1;
+			placePiece(gameBoard, cpuPos, "cpu");
+			
+			System.out.println("Bot's turn:");
+			printGameBoard(gameBoard);
+		}
 		
-		placePiece(gameBoard, pos, "player");
-		printGameBoard(gameBoard);
-		
-		Random rand = new Random();
-		int cpuPos = rand.nextInt(9) + 1;
-		placePiece(gameBoard, cpuPos, "cpu");
-		
-		System.out.println("Bot's turn:");
-		printGameBoard(gameBoard);
+
 		
 
 	}
@@ -85,6 +95,20 @@ public class TicTacToe {
 		default:
 			break;
 		}
+	}
+	
+	public static String checkWinner() {
+		List topRow = Arrays.asList(1, 2, 3);
+		List midRow = Arrays.asList(4,5,6);
+		List botRow = Arrays.asList(7,8,9);
+		List leftCol = Arrays.asList(1,4,7);
+		List midCol = Arrays.asList(2,5,8);
+		List rightCol = Arrays.asList(3,6,9);
+		List cross1 = Arrays.asList(1,5,9);
+		List cross2 = Arrays.asList(3,5,7);
+		return null;
+		
+		
 	}
 	
 }
